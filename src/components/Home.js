c1b67fa067b45c862597b0d0771bf50d338b7753
zeeshanchana95 <zeeshanchana95@gmail.com>
2023-06-24 17:06:07 +0500

    Add Navbar ShowRecipees and Home components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from './src/components/Navbar'
-import ShowRecipees from './src/components/ShowRecipees'
+import Navbar from './Navbar'
+import ShowRecipees from './ShowRecipees'
 
 export const Home = () => {
   return (
